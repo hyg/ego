72b49c6b32ba2bd1a795aa14be92a2ea647c4d44
huangyg <huangyg@mars22.com>
2013-03-19 07:28:42 +0800

    page design

---
+++ doc/web.rp/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u)]),_(c,v,e,f,g,w),_(c,x,e,f,g,y,i,[_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E)])]);}; 
+var b="rootNodes",c="pageName",d="菜单框架",e="type",f="Wireframe",g="url",h="菜单框架.html",i="children",j="契约清单",k="契约清单.html",l="契约详情",m="契约详情.html",n="信息源",o="信息源.html",p="信息页",q="信息页.html",r="账目概况",s="账目概况.html",t="账目详情",u="账目详情.html",v="流程模式",w="流程模式.html",x="独立页",y="独立页.html",z="注册",A="注册.html",B="登录",C="登录.html",D="规则",E="规则.html";
+return _creator();
+})();
