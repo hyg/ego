01b0b8dcb5fad510268aa56d08b41be0d5e5b7fa
huangyg <huangyg@mars22.com>
2013-03-19 19:22:30 +0800

    add some pages

--- doc/web.rp/信息源_files/axurerp_pagespecificscript.js
+++ doc/web.rp/信息源_files/axurerp_pagespecificscript.js
@@ -1,6 +1,27 @@
-﻿for(var i = 0; i < 0; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 5; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
+
+u3.style.cursor = 'pointer';
+$axure.eventManager.click('u3', function(e) {
+
+if (true) {
+
+	self.location.href='http://weibo.com/huangyg';
+
+}
+});
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href='http://blog.sina.com.cn/mars22';
+
+}
+});
+gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u2'] = 'top';
