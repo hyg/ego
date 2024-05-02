9d3cf50d6412ebee442e6ec82179fd35a17660a9
Huang,Yonggang <huangyg@mars22.com>
2024-05-02 17:11:51 +0800

    - task:PSMD  [error,knowledge metadata鐨刾laceholder鍗囩骇](../../../draft/2024/05/20240502074500.md)\n- task:PSMD  [term metadata -> term html](../../../draft/2024/05/20240502093000.md)\n- task:ego  [鎶妛aitinglist鍓嶅嚑鍚嶅拰娆℃棩鍙兘鐨勬ā鐗堢粦瀹氱粨鏋滃啓鍏ユ棩灏忕粨銆俔(../../../draft/2024/05/20240502140000.md)\n- task:js  [娴嬭瘯](../../../draft/2024/05/20240502143000.md)\n- task:learn  [鎶奼it寮€鍙戞祦绋嬬紪鍐欐垚鎵瑰鐞嗘枃浠禲(../../../draft/2024/05/20240502160000.md)

--- src/finish.bat
+++ src/finish.bat
@@ -1,6 +1,25 @@
+set message="- task:PSMD  [error,knowledge metadata的placeholder升级](../../../draft/2024/05/20240502074500.md)\n- task:PSMD  [term metadata -> term html](../../../draft/2024/05/20240502093000.md)\n- task:ego  [把waitinglist前几名和次日可能的模版绑定结果写入日小结。](../../../draft/2024/05/20240502140000.md)\n- task:js  [测试](../../../draft/2024/05/20240502143000.md)\n- task:learn  [把git开发流程编写成批处理文件](../../../draft/2024/05/20240502160000.md)"
 cd ..\..\ego
 git add .
-git commit -m "- task:PSMD  [error,knowledge metadata的placeholder升级](../../../draft/2"
+git commit -m %message%
 git push github
 git push coding
+git push gitee
+cd ..\PSMD
+git add .
+git commit -m %message%
+git push github
+git push codeberg
+git push gitee
+cd ..\blog
+git add .
+git commit -m %message%
+git push github
+git push codeberg
+git push gitee
+cd ..\draft
+git add .
+git commit -m %message%
+git push github
+git push codeberg
 git push gitee
