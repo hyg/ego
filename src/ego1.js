b2a25daf16646f988537acd7412fa8992a800b14
Huang,Yonggang <huangyg@mars22.com>
2024-06-12 17:26:49 +0800

    - 07:45	[格里申法案](#20240612074500) - 09:30	[ego draft, metadata](#20240612093000) - 14:00	[学习AMD规范，如果适用就开发范例](#20240612140000) - 14:30	[用commander的.command()或.addCommand()改写入口](#20240612143000) - 16:00	[linkml, yaml-ld](#20240612160000)

--- src/ego1.js
+++ src/ego1.js
@@ -9,25 +9,39 @@ const { Command } = require('commander');
 const program = new Command();
 
 const log = s => console.log(s);
+const debug = false;
+start.debug = debug;
+finish.debug = debug;
 
 program
   .name('ego')
   .description('个人领域的理性部分')
   .version('0.1.1');
 
-  program.command('day')
+program.command('day')
   .description('以天为单位的自我管理功能')
   .option('-i, --init <int>', '初始化：绑定时间模版，创建日计划、次日规划、手稿及元数据文件。')
   .option('-o, --over', '工作结束，生成日小结、更新次日规划。')
   .option('-p, --plan', '显示次日规划，不更新任何文件。')
   .action((options) => {
     log(options);
-    if(options.init){
-        log("init:",options.init);
-    }else if(options.over){
-        log("over:",options.over);
-    }else if(options.plan){
-        log("plan:",options.plan);
+    if (options.init) {
+      var date = util.datestr();
+      var tomorrow = util.datestr(1);
+      var plan = options.init;
+      start.makedaydraft(date, plan);
+      start.makedayplan(date);
+      finish.maketomorowinfo(tomorrow);
+    } else if (options.over) {
+      var date = util.datestr();
+      var tomorrow = util.datestr(1);
+      finish.updateseason(date);
+      finish.makedaylog(date);
+      finish.maketomorowinfo(tomorrow);
+    } else if (options.plan) {
+      var date = util.datestr();
+      finish.updateseason(date);
+      start.testdayplan();
     }
   });
 
