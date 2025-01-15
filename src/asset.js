77788c0f599b925e0830127da5593bde4591cf0c
黄勇刚 <huangyg@mars22.com>
2025-01-15 21:06:15 +0800

    - 07:45~12:14	PSMD: [智在科技项目准备](#20250115074500) - 14:00~14:00	PSMD: [整理新版本term的manifest code](#20250115140000) - 16:00~17:29	PSMD: [整理近期讨论，按照新版manifest text提炼几个有代表性的场景。](#20250115160000)

--- src/asset.js
+++ src/asset.js
@@ -31,7 +31,7 @@ module.exports = {
                 record.amount = item.amount;
                 Account[item.AccountTitle].record.push(record);
                 Account[item.AccountTitle].balance[item.asset] += item.amount ;
-
+                Account[item.AccountTitle].balance[item.asset] = Math.round((Account[item.AccountTitle].balance[item.asset]) * 100) / 100 ;
             }for(var id in AER.AccountingEntry.credit){
                 var item = AER.AccountingEntry.credit[id];
                 if(Account[item.AccountTitle].record == undefined){
@@ -45,6 +45,7 @@ module.exports = {
                 record.amount = item.amount;
                 Account[item.AccountTitle].record.push(record);
                 Account[item.AccountTitle].balance[item.asset] -= item.amount ;
+                Account[item.AccountTitle].balance[item.asset] = Math.round((Account[item.AccountTitle].balance[item.asset]) * 100) / 100 ;
             }
         }
         log(yaml.dump(Account));
@@ -65,6 +66,6 @@ module.exports = {
         return {"rmb": {id: 1,name: "rmb"}};
     },
     loadAccount(){
-        return {"银行存款": {id: 1, name: "银行存款",balance: {"rmb": 0}},"现金.微信零钱": {id: 2.1, name: "现金.微信零钱",balance: {"rmb": 0}},"raw": {id: 10,name: "raw",balance: {"rmb": 0}},"raw.food": {id: 10.1,name: "raw.food",balance: {"rmb": 0}},"raw.med": {id: 10.2,name: "raw.med",balance: {"rmb": 0}},"raw.site": {id: 10.3,name: "raw.site",balance: {"rmb": 0}},"raw.site.bj1": {id: "10.3.1.",name: "raw.site.bj1",balance: {"rmb": 0}},"raw.site.wz": {id: "10.3.2.",name: "raw.site.wz",balance: {"rmb": 0}},"donation": {id: 20,name: "donation",balance: {"rmb": 0}},"donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0}},"donation.younger": {id: 20.2,name: "donation.younger",balance: {"rmb": 0}},"donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0}},"PSMD": {id: 100,name: "PSMD",balance: {"rmb": 0}}};
+        return {"银行存款": {id: 1, name: "银行存款",balance: {"rmb": 0.0}},"现金.微信零钱": {id: 2.1, name: "现金.微信零钱",balance: {"rmb": 0.0}},"raw": {id: 10,name: "raw",balance: {"rmb": 0.0}},"raw.food": {id: 10.1,name: "raw.food",balance: {"rmb": 0.0}},"raw.med": {id: 10.2,name: "raw.med",balance: {"rmb": 0.0}},"raw.site": {id: 10.3,name: "raw.site",balance: {"rmb": 0.0}},"raw.site.bj1": {id: "10.3.1.",name: "raw.site.bj1",balance: {"rmb": 0.0}},"raw.site.wz": {id: "10.3.2.",name: "raw.site.wz",balance: {"rmb": 0.0}},"donation": {id: 20,name: "donation",balance: {"rmb": 0.0}},"donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0.0}},"donation.younger": {id: 20.2,name: "donation.younger",balance: {"rmb": 0.0}},"donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0.0}},"PSMD": {id: 100,name: "PSMD",balance: {"rmb": 0.0}}};
     }
 }
