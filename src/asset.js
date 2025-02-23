627818a4f20b031c439c383a3972b9f783d1dcf9
hyg <huangyg@mars22.com>
2025-02-24 02:14:11 +0800

    - 17:31~18:15	PSMD: [meta data微调](#20250223173100) - 19:30~00:29	ego: [手稿从simplenote转移到飞书的设计](#20250223193000)

--- src/asset.js
+++ src/asset.js
@@ -71,17 +71,18 @@ module.exports = {
             "银行存款": {id: 1, name: "银行存款",balance: {"rmb": 0.0}},
             "现金.微信零钱": {id: 2.1, name: "现金.微信零钱",balance: {"rmb": 0.0}},
             "raw": {id: 10,name: "raw",balance: {"rmb": 0.0}},
-                "raw.food": {id: 10.1,name: "raw.food",balance: {"rmb": 0.0}},
-                "raw.med": {id: 10.2,name: "raw.med",balance: {"rmb": 0.0}},
-                "raw.site": {id: 10.3,name: "raw.site",balance: {"rmb": 0.0}},
-                "raw.site.bj1": {id: "10.3.1.",name: "raw.site.bj1",balance: {"rmb": 0.0}},
-                "raw.site.wz": {id: "10.3.2.",name: "raw.site.wz",balance: {"rmb": 0.0}},
-                "raw.fun": {id: 10.4,name: "raw.fun",balance: {"rmb": 0.0}},
-                "donation": {id: 20,name: "donation",balance: {"rmb": 0.0}},
-                "donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0.0}},
-                "donation.younger": {id: 20.2,name: "donation.younger",balance: {"rmb": 0.0}},
-                "PSMD": {id: 100,name: "PSMD",balance: {"rmb": 0.0}},
-                "xuemen": {id: 1000,name: "xuemen",balance: {"rmb": 0.0}}
+            "raw.food": {id: 10.1,name: "raw.food",balance: {"rmb": 0.0}},
+            "raw.med": {id: 10.2,name: "raw.med",balance: {"rmb": 0.0}},
+            "raw.site": {id: 10.3,name: "raw.site",balance: {"rmb": 0.0}},
+            "raw.site.bj1": {id: "10.3.1.",name: "raw.site.bj1",balance: {"rmb": 0.0}},
+            "raw.site.wz": {id: "10.3.2.",name: "raw.site.wz",balance: {"rmb": 0.0}},
+            "raw.fun": {id: 10.4,name: "raw.fun",balance: {"rmb": 0.0}},
+            "raw.shell": {id: 10.5,name: "raw.shell",balance: {"rmb": 0.0}},
+            "donation": {id: 20,name: "donation",balance: {"rmb": 0.0}},
+            "donation.parent": {id: 20.1,name: "donation.parent",balance: {"rmb": 0.0}},
+            "donation.younger": {id: 20.2,name: "donation.younger",balance: {"rmb": 0.0}},
+            "PSMD": {id: 100,name: "PSMD",balance: {"rmb": 0.0}},
+            "xuemen": {id: 1000,name: "xuemen",balance: {"rmb": 0.0}}
             };
         return account;
     }
